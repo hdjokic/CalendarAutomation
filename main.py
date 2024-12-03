3a0b7ff02d4b9487f7be03e0c47b53f24228319d
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2024-12-03 13:50:52 -0600

    get spreadsheet data

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import os.path
+import os
 import datetime as dt
 
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import (Credentials)
+from google.oauth2.credentials import Credentials
 
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SPREADSHEET_ID = "1ndzqhxWEzciYuENn6DwitdF5ex_LJBdP4HdClSc2tNw"
 
 def main():
 
     #how to authenticate
     creds = None
     if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json")
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
     #if no creds available let user sign in
     if not creds or not creds.valid:
@@ -30,29 +31,35 @@ def main():
 
     try:
         #get calendar service
-        service = build("calendar", "v3", credentials = creds)
+        service = build("sheets", "v4", credentials = creds)
+        sheets = service.spreadsheets()
+        result = sheets.values().get(spreadsheetId = SPREADSHEET_ID, range = "SubSheet 2024!A1:B1").execute()
 
+        values = result.get("values", [])
+
+        for row in values:
+            print(row)
         #add an event to the calendar----------------------------------------
-        event = {
-            "summary": "My python event",
-            "location": "Online",
-            "description": "details",
-            "colorId": 6,
-            "start":{
-                "dateTime": "2024-12-02T09:00:00+02:00",
-                "timeZone": "America/Chicago"
-            },
-            "end": {
-                "dateTime": "2024-12-02T12:00:00+02:00",
-                "timeZone": "America/Chicago"
-            },
-            "attendees":[
-                {"email": "hristtina99@gmail.com"}
-            ]
-
-        }
-        event = service.events().insert(calendarId="primary", body = event).execute()
-        print(f"Event created {event.get('htmlLink')}")
+        # event = {
+        #     "summary": "My python event",
+        #     "location": "Online",
+        #     "description": "details",
+        #     "colorId": 6,
+        #     "start":{
+        #         "dateTime": "2024-12-02T09:00:00+02:00",
+        #         "timeZone": "America/Chicago"
+        #     },
+        #     "end": {
+        #         "dateTime": "2024-12-02T12:00:00+02:00",
+        #         "timeZone": "America/Chicago"
+        #     },
+        #     "attendees":[
+        #         {"email": "hristtina99@gmail.com"}
+        #     ]
+        #
+        # }
+        # event = service.events().insert(calendarId="primary", body = event).execute()
+        # print(f"Event created {event.get('htmlLink')}")
 
     except HttpError as error:
         print("An error occured: ", error)
