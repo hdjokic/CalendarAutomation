f9cb183ef481ed2732efb9330af18040b90db76e
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2024-12-03 10:25:54 -0600

    if creds are not found

--- main.py
+++ main.py
@@ -17,8 +17,15 @@ def main():
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json")
 
+    #if no creds available let user sign in
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port = 0)
+        #save creds to token.json
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
 
