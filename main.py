28bc53adc0b3d21dacd5ebd8d4b7c5cc680a2631
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2024-12-04 11:56:25 -0600

    add basic case to calendar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import datetime as dt
-
+import datetime
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
@@ -8,11 +8,19 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly", "https://www.googleapis.com/auth/calendar"]
 SPREADSHEET_ID = "1ndzqhxWEzciYuENn6DwitdF5ex_LJBdP4HdClSc2tNw"
 
-def main():
 
+#add event to calendar
+def add_event_to_calendar(service, event_data):
+    try:
+        event = service.events().insert(calendarId="primary", body=event_data).execute()
+        print(f"Event created: {event.get('htmlLink')}")
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+def main():
     #how to authenticate
     creds = None
     if os.path.exists("token.json"):
@@ -32,61 +40,35 @@ def main():
     try:
         #get calendar service
         service = build("sheets", "v4", credentials = creds)
+        calendarService = build("calendar", "v3", credentials=creds)
         sheets = service.spreadsheets()
-        result = sheets.values().get(spreadsheetId = SPREADSHEET_ID, range = "SubSheet 2024!A1:B1").execute()
+
+        result = sheets.values().get(spreadsheetId = SPREADSHEET_ID, range = "Sheet45!A2:ZZ").execute()
 
         values = result.get("values", [])
 
         for row in values:
-            print(row)
-        #add an event to the calendar----------------------------------------
-        # event = {
-        #     "summary": "My python event",
-        #     "location": "Online",
-        #     "description": "details",
-        #     "colorId": 6,
-        #     "start":{
-        #         "dateTime": "2024-12-02T09:00:00+02:00",
-        #         "timeZone": "America/Chicago"
-        #     },
-        #     "end": {
-        #         "dateTime": "2024-12-02T12:00:00+02:00",
-        #         "timeZone": "America/Chicago"
-        #     },
-        #     "attendees":[
-        #         {"email": "hristtina99@gmail.com"}
-        #     ]
-        #
-        # }
-        # event = service.events().insert(calendarId="primary", body = event).execute()
-        # print(f"Event created {event.get('htmlLink')}")
+            start_time, end_time = row[5].split('-')
+            print(start_time)
+            print(end_time)
+            start_time = dt.datetime.strptime(start_time.strip(), "%H")
+            end_time = dt.datetime.strptime(end_time.strip(), "%H")
+
+            start_time = start_time.replace(year=dt.date.today().year, month=dt.date.today().month,day=dt.date.today().day)
+            end_time = end_time.replace(year=dt.date.today().year, month=dt.date.today().month, day=dt.date.today().day)
+            if row[6] == "Hristina P":
+                event_data = {
+                    'summary': 'LSF tennis',
+                    'class category': row[1],
+                    'start': {'dateTime': start_time.isoformat(), 'timeZone': 'America/Chicago'},
+                    'end': {'dateTime': end_time.isoformat(), 'timeZone': 'America/Chicago'},
+                    'attendees': [{'email':"hristtina99@gmail.com"}]
+                }
+                print(event_data)
+                add_event_to_calendar(calendarService, event_data)
 
     except HttpError as error:
         print("An error occured: ", error)
-
-        # LIST 10 EVENTS --------------------------------------------------
-        # now = dt.datetime.now().isoformat() + "Z"
-        #
-        # events_result = (
-        #     service.events()
-        #     .list(
-        #         calendarId="primary",
-        #         timeMin = now,
-        #         maxResults=10,
-        #         singleEvents=True,
-        #         orderBy="startTime",
-        #    )
-        #     .execute()
-        # )
-        # events = events_result.get("items", [])
-        #
-        # if not events:
-        #     print("No upcoming events found")
-        #     return
-        # for event in events:
-        #     start = event["start"].get("dateTime", event["start"].get("date"))
-        #     print(start, event["summary"])
-        # LIST 10 EVENTS--------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
